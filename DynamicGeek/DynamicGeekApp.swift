93de559105beee7ca29b22c335817734606fc9f6
Victor Barrios <victor.barrios@bestsecret.com>
2022-10-28 15:38:06 +0200

    Initial Commit

---
+++ DynamicGeek/DynamicGeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicGeekApp.swift
+//  DynamicGeek
+//
+//  Created by Barrios, Victor on 28/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DynamicGeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
